18364fed224ff913fd739697d52f3b893745cecf
Masuduzzaman <md.santo0583210@gmail.com>
2026-07-07 14:48:10 +0600

    create jwt utils and add it loginUser function

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -2,6 +2,8 @@ import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import config from "../../config";
 import { ILoginUser, RegisterUserPayload } from "./auth.interface";
+import { jwtUtils } from "../../utils/jwt";
+import { SignOptions } from "jsonwebtoken";
 
 const registerUserIntoDb = async (payload: RegisterUserPayload) => {
   const { name, email, password, profilePhoto } = payload;
@@ -9,8 +11,8 @@ const registerUserIntoDb = async (payload: RegisterUserPayload) => {
     where: { email },
   });
   if (isUserExist) {
-        throw new Error("User with this email already exists");
-    }
+    throw new Error("User with this email already exists");
+  }
 
   const hashedPassword = await bcrypt.hash(
     password,
@@ -31,39 +33,65 @@ const registerUserIntoDb = async (payload: RegisterUserPayload) => {
   });
 
   const user = await prisma.user.findUnique({
-    where:{
-        id: createUser.id,
-        email: createUser.email || email,
+    where: {
+      id: createUser.id,
+      email: createUser.email || email,
     },
-    omit:{
-        password: true,
+    omit: {
+      password: true,
     },
-    include:{
-        profile: true,
-    }
-  })
+    include: {
+      profile: true,
+    },
+  });
   return user;
 };
 
 const loginUserIntoDb = async (payload: ILoginUser) => {
-    const { email, password } = payload;
+  const { email, password } = payload;
 
-    const user = await prisma.user.findUniqueOrThrow({
-        where:{email},
-    })
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
 
-    if(user.activeStatus === "SUSPENDED"){
-        throw new Error("Your account has been suspended. Please contact support for assistance.");
+  if (user.activeStatus === "SUSPENDED") {
+    throw new Error(
+      "Your account has been suspended. Please contact support for assistance.",
+    );
+  }
+
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatched) {
+    throw new Error("Password does not match");
+  }
+
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_access_token_secret,
+        config.jwt_access_expires_in as SignOptions
+    );
+
+    const refreshToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_refresh_token_secret,
+        config.jwt_refresh_expires_in as SignOptions
+    );
+
+    return {
+        accessToken,
+        refreshToken,
     }
-
-    const isPasswordMatched = await bcrypt.compare(password, user.password);
-
-    if(!isPasswordMatched){
-        throw new Error("Password does not match");
-}
-}
+};
 
 export const authService = {
-    registerUserIntoDb,
-    loginUserIntoDb,
-}
+  registerUserIntoDb,
+  loginUserIntoDb,
+};
